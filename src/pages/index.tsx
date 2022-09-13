cef63575e806e9c24c51cca6442a5775018c5765
Kydollar <rizkywidodocuan@gmail.com>
2022-09-13 11:50:43 +0700

    setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import ButtonPrimary from 'components/Button/primary'
+import ButtonSecondary from 'components/Button/secondary'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      TEST
-    </div>
+    <section className="relative">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="my-6">
+            <h1 className="text-3xl font-bold text-zinc-700">@KYDOLLAR</h1>
+          </div>
+          <div className="my-6">
+            <ButtonPrimary onClick={() => alert('This is button primary!')} inputClassName="mx-4">Primary</ButtonPrimary>
+            <ButtonSecondary onClick={() => alert('This is button secondary!')} inputClassName="mx-4">Secondary</ButtonSecondary>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
