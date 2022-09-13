c7c6c2744d3fd6415934d506f2f68ea8a70c4042
Kydollar <rizkywidodocuan@gmail.com>
2022-09-13 11:50:35 +0700

    add component button

--- src/components/Button/primary.tsx
+++ src/components/Button/primary.tsx
@@ -1,12 +1,12 @@
 import { ButtonProps } from "types/button";
 
-export default function Button(props: ButtonProps) {
+export default function ButtonPrimary(props: ButtonProps) {
     const { onClick, children, inputClassName } = props;
 
     return (
         <button
             className={[
-                "py-4 px-16 rounded-2xl text-sm text-white font-semibold tracking-wide subpixel-antialiased shadow-xl",
+                "bg-blue-500 py-4 px-16 rounded-full text-sm text-white font-semibold tracking-wide subpixel-antialiased shadow-xl",
                 inputClassName,
             ].join(" ")}
             onClick={onClick}
--- src/components/Button/secondary.tsx
+++ src/components/Button/secondary.tsx
@@ -1,12 +1,12 @@
 import { ButtonProps } from "types/button";
 
-export default function Button(props: ButtonProps) {
+export default function ButtonSecondary(props: ButtonProps) {
     const { onClick, children, inputClassName } = props;
 
     return (
         <button
             className={[
-                "py-4 px-16 rounded-2xl text-sm text-white font-semibold tracking-wide subpixel-antialiased shadow-xl",
+                "bg-green-500 py-4 px-16 rounded-full text-sm text-white font-semibold tracking-wide subpixel-antialiased shadow-xl",
                 inputClassName,
             ].join(" ")}
             onClick={onClick}
